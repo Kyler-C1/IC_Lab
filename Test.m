657f53d5a40fd88c6507b8cdd65d5266ba20f652
Kyler-C1 <kjclark2@ou.edu>
2023-04-10 18:33:52 -0500

    fixed title sizes and torque gauge

--- Test.m
+++ Test.m
@@ -1,11 +1,10 @@
 %%set_param('dashboardv2/Subsystem/dashboardv2/Subsystem/Intake Air Flow Rate (g/s)','Name', 'Fuel Mass Flow Rate (g/s)');
-get_param('dashboardv2/Subsystem/Engine Selection','ObjectParameters')
+get_param('dashboardv2/Subsystem/Intake Air Flow Rate (kg//s)','ObjectParameters')
 size = 25;
 set_param('dashboardv2/Subsystem/Intake Air Flow Rate (kg//s)','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/Fuel Mass Flow Rate (g//s)','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/BMEP (kPa)','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/Thermal Efficiency','FontSize', num2str(size));
-set_param('dashboardv2/Subsystem/Engine Torque (Nm)','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/Brake Power (kW)','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/Volumetric Efficiency','FontSize', num2str(size));
 set_param('dashboardv2/Subsystem/BSFC (kg//kJ)','FontSize', num2str(size));
@@ -14,4 +13,9 @@ set_param('dashboardv2/Subsystem/Engine Speed (RPM)','FontSize', num2str(size));
 %%set_param('dashboardv2/Subsystem/Radio Button','Name', 'Unit Selection');
 set_param('dashboardv2/Subsystem/Engine Selection','FontSize',num2str(size));
 set_param('dashboardv2/Subsystem/Unit Selection','FontSize',num2str(size));
+set_param('dashboardv2/Subsystem/Torque (Nm)','FontSize',num2str(size));
+set_param(strcat('dashboardv2/Subsystem/','Temp (', char(176),'C)'),'FontSize',num2str(size));
+
+%{
 set_param(strcat('dashboardv2/Subsystem/Exhaust Temp (', char(176), 'C)'),'FontSize', num2str(size));
+%}
